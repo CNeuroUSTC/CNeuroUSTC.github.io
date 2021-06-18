4050554982bc973e2a69cc543ebaf86cd2b9f70c
xrk <earth@mail.ustc.edu.cn>
2021-06-18 19:07:49 +0800

    Create autoJC.py

---
+++ _data/autoJC.py
@@ -0,0 +1,4 @@
+import easygui as JCgui
+#JCgui.msgbox(msg='Please Input JC Information', title='JC Information Update', ok_button='OK', image=None, root=None)
+JCgui.ccbox(msg='Please Input JC Information', title='JC Information Update', choices=('Weekly JC','Theory JC'), image=None)
+
