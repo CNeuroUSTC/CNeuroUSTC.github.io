40fcd7e15b4a795882a7e3c2263bfcc72b072e23
RK <earth@mail.ustc.edu.cn>
2021-07-26 17:55:37 +0800

    .gitignore

--- _data/autoJC.py
+++
@@ -1,4 +0,0 @@
-import easygui as JCgui
-#JCgui.msgbox(msg='Please Input JC Information', title='JC Information Update', ok_button='OK', image=None, root=None)
-JCgui.ccbox(msg='Please Input JC Information', title='JC Information Update', choices=('Weekly JC','Theory JC'), image=None)
-
