60e9fd40d87affea69174fbcf45fd819bbfc2e0c
cyk-git <cyk_phy@mail.ustc.edu.cn>
2021-11-12 14:53:41 +0800

    test7

--- Weekly_JC_md/assets/js/mailme.js
+++
@@ -1,2 +0,0 @@
-zisafunnytrickfromalex =('slewallen'+'@'+'fas'+'.harvard'+'.edu')
-document.write('<a class="contact" href="mailto:' + zisafunnytrickfromalex + '">'+ zisafunnytrickfromalex + '</a>')
